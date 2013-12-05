a62957fda966ab605c5cd263e0ffef4844b751cf
Adam Schwartz <aschwartz@hubspot.com>
2013-12-05 00:02:37 -0500

    Fun pass at lively tether welcome docs

--- docs/welcome/coffee/welcome.coffee
+++ docs/welcome/coffee/welcome.coffee
@@ -1,1 +1,64 @@
-return
+init = ->
+    setupDemo()
+
+setupDemo = ->
+    $button = $('.button.demo')
+
+    positions = [
+        'top left'
+        'left top'
+        'left bottom'
+        'bottom left'
+        'bottom right'
+        'right bottom'
+        'right top'
+        'top right'
+    ]
+
+    window.drops = {}
+
+    for position in positions
+        drops[position] = new Drop
+            target: $button[0]
+            className: 'drop-theme-arrows'
+            attach: position
+            constrainToScrollParent: true
+            trigger: 'manual' # Just not click
+            content: $.map(position.split(' '), (word) -> word.substr(0, 1).toUpperCase() + word.substr(1)).join(' ')
+
+        # TODO - remove once zbloom fixes
+        drops[position].$drop.addClass "drop-attached-#{ position.replace(' ', '-')}"
+
+    openIndex = 0
+    frames = 0
+    frameLengthMS = 6
+
+    openAllDrops = ->
+        for position, drop of drops
+            drop.open()
+
+        drops['top right'].close()
+        drops['bottom left'].close()
+
+    openNextDrop = ->
+        for position, drop of drops
+            drop.close()
+
+        drops[positions[openIndex]].open()
+        drops[positions[(openIndex + 4) % positions.length]].open()
+
+        openIndex = (openIndex + 1) % positions.length
+
+        if frames > 40
+            return openAllDrops()
+
+        frames += 1
+
+        setTimeout openNextDrop, frameLengthMS * frames
+
+    openNextDrop()
+
+    # $(document).on 'dropopen closedrop', (event) ->
+    #     log 'event', 'type', event.type, 'drop', event
+
+init()
--- docs/welcome/js/welcome.js
+++ docs/welcome/js/welcome.js
@@ -1,3 +1,59 @@
 (function() {
-  return;
+  var init, setupDemo;
+
+  init = function() {
+    return setupDemo();
+  };
+
+  setupDemo = function() {
+    var $button, frameLengthMS, frames, openAllDrops, openIndex, openNextDrop, position, positions, _i, _len;
+    $button = $('.button.demo');
+    positions = ['top left', 'left top', 'left bottom', 'bottom left', 'bottom right', 'right bottom', 'right top', 'top right'];
+    window.drops = {};
+    for (_i = 0, _len = positions.length; _i < _len; _i++) {
+      position = positions[_i];
+      drops[position] = new Drop({
+        target: $button[0],
+        className: 'drop-theme-arrows',
+        attach: position,
+        constrainToScrollParent: true,
+        trigger: 'manual',
+        content: $.map(position.split(' '), function(word) {
+          return word.substr(0, 1).toUpperCase() + word.substr(1);
+        }).join(' ')
+      });
+      drops[position].$drop.addClass("drop-attached-" + (position.replace(' ', '-')));
+    }
+    openIndex = 0;
+    frames = 0;
+    frameLengthMS = 6;
+    openAllDrops = function() {
+      var drop;
+      for (position in drops) {
+        drop = drops[position];
+        drop.open();
+      }
+      drops['top right'].close();
+      return drops['bottom left'].close();
+    };
+    openNextDrop = function() {
+      var drop;
+      for (position in drops) {
+        drop = drops[position];
+        drop.close();
+      }
+      drops[positions[openIndex]].open();
+      drops[positions[(openIndex + 4) % positions.length]].open();
+      openIndex = (openIndex + 1) % positions.length;
+      if (frames > 40) {
+        return openAllDrops();
+      }
+      frames += 1;
+      return setTimeout(openNextDrop, frameLengthMS * frames);
+    };
+    return openNextDrop();
+  };
+
+  init();
+
 }).call(this);
