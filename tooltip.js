f033796f6e2b3615aab324d859a4a8eb571d29d6
Adam Schwartz <aschwartz@hubspot.com>
2013-12-10 15:52:16 -0500

    Add tooltip.js

---
+++ tooltip.js
@@ -0,0 +1,38 @@
+(function() {
+  var DropTooltip, Tooltip, defaults;
+
+  DropTooltip = Drop.createContext();
+
+  defaults = {
+    attach: 'top center'
+  };
+
+  Tooltip = (function() {
+    function Tooltip(options) {
+      this.options = options;
+      this.$target = $(this.options.el);
+      this.createDrop();
+    }
+
+    Tooltip.prototype.createDrop = function() {
+      if (this.options.attach == null) {
+        this.options.attach = defaults.attach;
+      }
+      return this.dropTooltip = new DropTooltip({
+        target: this.$target[0],
+        className: 'drop-tooltip-theme-arrows',
+        attach: this.options.attach,
+        constrainToWindow: true,
+        constrainToScrollParent: false,
+        openOn: 'hover',
+        content: this.$target.attr('data-tooltip-content')
+      });
+    };
+
+    return Tooltip;
+
+  })();
+
+  window.Tooltip = Tooltip;
+
+}).call(this);
