6cb3d4a411139faa4dc761e8eda647571e01c8a2
Adam Schwartz <aschwartz@hubspot.com>
2014-01-16 00:18:25 -0500

    v0.3.6

--- tether.js
+++ tether.js
@@ -1,4 +1,4 @@
-/*! tether 0.3.5 */
+/*! tether 0.3.6 */
 (function() {
   var Evented, addClass, extend, getBounds, getOffsetParent, getScrollParent, hasClass, removeClass,
     __hasProp = {}.hasOwnProperty,
