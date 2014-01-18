c5c9cd8fb5e1c0f53b4d6744f208bcfcd7ddedef
Zack Bloom <zbloom@hubspot.com>
2014-01-18 01:10:42 -0500

    Version bump

--- tether.js
+++ tether.js
@@ -1,4 +1,4 @@
-/*! tether 0.4.3 */
+/*! tether 0.4.4 */
 (function() {
   var Evented, addClass, defer, deferred, extend, flush, getBounds, getOffsetParent, getOrigin, getScrollParent, hasClass, node, removeClass, uniqueId, updateClasses, zeroPosCache,
     __hasProp = {}.hasOwnProperty,
