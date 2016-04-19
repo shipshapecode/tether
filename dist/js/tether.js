996ce979854a7bb53573069391921a6f8c2305f5
Trevor Burnham <tburnham@hubspot.com>
2016-04-19 18:11:59 -0400

    Rebuild docs for 1.2.3
    Trying to pin down what went wrong with the 1.3.0 release...

--- dist/js/tether.js
+++ dist/js/tether.js
@@ -1,4 +1,4 @@
-/*! tether 1.3.0 */
+/*! tether 1.2.3 */
 
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
@@ -33,7 +33,7 @@ function getScrollParents(el) {
   }
 
   var parent = el;
-  while ((parent = parent.parentNode) && parent && parent.nodeType === 1) {
+  while (parent = parent.parentNode) {
     var style = undefined;
     try {
       style = getComputedStyle(parent);
@@ -384,11 +384,7 @@ var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = 
 
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-var _get = function get(_x6, _x7, _x8) { var _again = true; _function: while (_again) { var object = _x6, property = _x7, receiver = _x8; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x6 = parent; _x7 = property; _x8 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 if (typeof TetherBase === 'undefined') {
   throw new Error('You must include the utils.js file before tether.js');
@@ -579,15 +575,12 @@ var parseOffset = function parseOffset(value) {
 };
 var parseAttachment = parseOffset;
 
-var TetherClass = (function (_Evented) {
-  _inherits(TetherClass, _Evented);
-
+var TetherClass = (function () {
   function TetherClass(options) {
     var _this = this;
 
     _classCallCheck(this, TetherClass);
 
-    _get(Object.getPrototypeOf(TetherClass.prototype), 'constructor', this).call(this);
     this.position = this.position.bind(this);
 
     tethers.push(this);
@@ -1194,7 +1187,7 @@ var TetherClass = (function (_Evented) {
       if (!moved) {
         var offsetParentIsBody = true;
         var currentNode = this.element.parentNode;
-        while (currentNode && currentNode.nodeType === 1 && currentNode.tagName !== 'BODY') {
+        while (currentNode && currentNode.tagName !== 'BODY') {
           if (getComputedStyle(currentNode).position !== 'static') {
             offsetParentIsBody = false;
             break;
@@ -1231,7 +1224,7 @@ var TetherClass = (function (_Evented) {
   }]);
 
   return TetherClass;
-})(Evented);
+})();
 
 TetherClass.modules = [];
 
@@ -1599,10 +1592,6 @@ TetherBase.modules.push({
 
       if (tAttachment.top !== targetAttachment.top || tAttachment.left !== targetAttachment.left || eAttachment.top !== _this.attachment.top || eAttachment.left !== _this.attachment.left) {
         _this.updateAttachClasses(eAttachment, tAttachment);
-        _this.trigger('update', {
-          attachment: eAttachment,
-          targetAttachment: tAttachment
-        });
       }
     });
 
