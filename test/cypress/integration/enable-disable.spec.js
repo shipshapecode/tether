0f6736bde12c448192fd8ea59928145927415ca9
Robert Wagner <rwwagner90@gmail.com>
2019-09-16 13:33:08 -0500

    Fix tests

--- test/cypress/integration/enable-disable.spec.js
+++ test/cypress/integration/enable-disable.spec.js
@@ -1,28 +1,30 @@
 describe('enable-disable test', () => {
-  describe('enable/disable works', () => {
-    it('tether-element should flip on resize', async () => {
-      await cy.visit('/examples/enable-disable/');
+  beforeEach(() => {
+    cy.visit('/examples/enable-disable/');
+  });
 
+  describe('enable/disable works', () => {
+    it('enable/disable should apply styles', () => {
       // Tether Enabled
-      await cy.get('.tether-target').should('have.class', 'tether-enabled');
-      await cy.get('.tether-element').should('have.attr', 'style')
+      cy.get('.tether-target').should('have.class', 'tether-enabled');
+      cy.get('.tether-element').should('have.attr', 'style')
         .should('contain', 'transform: translateX(710px) translateY(479px) translateZ(0px)');
 
-      await cy.get('.container').scrollTo(0, 250);
-      await cy.get('.tether-element').should('have.attr', 'style')
+      cy.get('.container').scrollTo(0, 250);
+      cy.get('.tether-element').should('have.attr', 'style')
         .should('contain', 'transform: translateX(710px) translateY(229px) translateZ(0px)');
-      await cy.get('.container').scrollTo(0, 0);
+      cy.get('.container').scrollTo(0, 0);
 
-      await cy.get('.tether-target').click();
+      cy.get('.tether-target').click();
 
       // Tether Disabled
-      await cy.get('.tether-target').should('not.have.class', 'tether-enabled');
-      await cy.get('.tether-element').should('have.attr', 'style')
+      cy.get('.tether-target').should('not.have.class', 'tether-enabled');
+      cy.get('.tether-element').should('have.attr', 'style')
         .should('contain', 'transform: translateX(710px) translateY(229px) translateZ(0px)');
-      await cy.get('.container').scrollTo(0, 250);
-      await cy.get('.tether-element').should('have.attr', 'style')
+      cy.get('.container').scrollTo(0, 250);
+      cy.get('.tether-element').should('have.attr', 'style')
         .should('contain', 'transform: translateX(710px) translateY(229px) translateZ(0px)');
-      await cy.get('.container').scrollTo(0, 0);
+      cy.get('.container').scrollTo(0, 0);
     });
   });
 });
