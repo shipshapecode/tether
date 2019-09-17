4deb42577520d55127748a3cb14c6efa0bdd4a7a
Robert Wagner <rwwagner90@gmail.com>
2019-09-17 13:28:10 -0400

    Try to set different values for CI

--- test/cypress/integration/enable-disable.spec.js
+++ test/cypress/integration/enable-disable.spec.js
@@ -5,14 +5,18 @@ describe('enable-disable test', () => {
 
   describe('enable/disable works', () => {
     it('enable/disable should apply styles', () => {
+      const isCI = Cypress.env('CI');
+      const x = isCI ? '1157' : '1170';
+      const y = isCI ? '228' : '229';
+      const enabledY = isCI ? '478' : '479';
       // Tether Enabled
       cy.get('.tether-target').should('have.class', 'tether-enabled');
       cy.get('.tether-element').should('have.attr', 'style')
-        .should('contain', 'transform: translateX(1170px) translateY(479px) translateZ(0px)');
+        .should('contain', `transform: translateX(${x}px) translateY(${enabledY}px) translateZ(0px)`);
 
       cy.get('.container').scrollTo(0, 250);
       cy.get('.tether-element').should('have.attr', 'style')
-        .should('contain', 'transform: translateX(1170px) translateY(229px) translateZ(0px)');
+        .should('contain', `transform: translateX(${x}px) translateY(${y}px) translateZ(0px)`);
       cy.get('.container').scrollTo(0, 0);
 
       cy.get('.tether-target').click();
@@ -20,10 +24,10 @@ describe('enable-disable test', () => {
       // Tether Disabled
       cy.get('.tether-target').should('not.have.class', 'tether-enabled');
       cy.get('.tether-element').should('have.attr', 'style')
-        .should('contain', 'transform: translateX(1170px) translateY(229px) translateZ(0px)');
+        .should('contain', `transform: translateX(${x}px) translateY(${y}px) translateZ(0px)`);
       cy.get('.container').scrollTo(0, 250);
       cy.get('.tether-element').should('have.attr', 'style')
-        .should('contain', 'transform: translateX(1170px) translateY(229px) translateZ(0px)');
+        .should('contain', `transform: translateX(${x}px) translateY(${y}px) translateZ(0px)`);
       cy.get('.container').scrollTo(0, 0);
     });
   });
