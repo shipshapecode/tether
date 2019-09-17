fbe3f9272eefa071e2e5c73d90d37448eb05421c
Robert Wagner <rwwagner90@gmail.com>
2019-09-17 18:42:00 -0400

    Try checking transforms again

--- test/cypress/integration/enable-disable.spec.js
+++ test/cypress/integration/enable-disable.spec.js
@@ -1,39 +1,28 @@
-import { assert } from 'chai';
-
 describe('enable-disable test', () => {
   beforeEach(() => {
     cy.visit('/examples/enable-disable/');
-    cy.get('.container').scrollTo(0, 0);
   });
 
   describe('enable/disable works', () => {
-    it('enable should apply transforms', () => {
+    it('enable/disable should apply styles', () => {
+      // Tether Enabled
       cy.get('.tether-target').should('have.class', 'tether-enabled');
-      cy.get('.tether-element').then((tetherElement) => {
-        const prescrollTransform = tetherElement[0].style.transform;
+      cy.get('.tether-element').should('have.attr', 'style')
+        .should('contain', 'transform: translateX(710px) translateY(479px) translateZ(0px);');
+      cy.get('.container').scrollTo(0, 250);
+      cy.get('.tether-element').should('have.attr', 'style')
+        .should('contain', 'transform: translateX(710px) translateY(229px) translateZ(0px);');
 
-        cy.get('.container').scrollTo(0, 250);
-        cy.wait(500);
-        cy.get('.tether-element').then((tetherElement) => {
-          const postscrollTransform = tetherElement[0].style.transform;
-          assert.notEqual(prescrollTransform, postscrollTransform);
-        });
-      });
-    });
-
-    it('disable should not apply transforms', () => {
       cy.get('.tether-target').click();
-      cy.get('.tether-target').should('not.have.class', 'tether-enabled');
-      cy.get('.tether-element').then((tetherElement) => {
-        const prescrollTransform = tetherElement[0].style.transform;
 
-        cy.get('.container').scrollTo(0, 250);
-        cy.wait(500);
-        cy.get('.tether-element').then((tetherElement) => {
-          const postscrollTransform = tetherElement[0].style.transform;
-          assert.equal(prescrollTransform, postscrollTransform);
-        });
-      });
+      // Tether Disabled
+      cy.get('.tether-target').should('not.have.class', 'tether-enabled');
+      cy.get('.tether-element').should('have.attr', 'style')
+        .should('contain', 'transform: translateX(710px) translateY(229px) translateZ(0px);');
+      cy.get('.container').scrollTo(0, 250);
+      cy.get('.tether-element').should('have.attr', 'style')
+        .should('contain', 'transform: translateX(710px) translateY(229px) translateZ(0px);');
+      cy.get('.container').scrollTo(0, 0);
     });
   });
 });
