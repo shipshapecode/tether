0255a531f105f248faa29bdbb6f826166237f7f5
Robert Wagner <rwwagner90@gmail.com>
2019-09-17 18:58:36 -0400

    Update enable-disable.spec.js

--- test/cypress/integration/enable-disable.spec.js
+++ test/cypress/integration/enable-disable.spec.js
@@ -5,23 +5,24 @@ describe('enable-disable test', () => {
 
   describe('enable/disable works', () => {
     it('enable/disable should apply styles', () => {
+      cy.viewport(700, 700);
       // Tether Enabled
       cy.get('.tether-target').should('have.class', 'tether-enabled');
       cy.get('.tether-element').should('have.attr', 'style')
-        .should('contain', 'transform: translateX(710px) translateY(479px) translateZ(0px);');
+        .should('contain', 'transform: translateX(560px) translateY(479px) translateZ(0px);');
       cy.get('.container').scrollTo(0, 250);
       cy.get('.tether-element').should('have.attr', 'style')
-        .should('contain', 'transform: translateX(710px) translateY(229px) translateZ(0px);');
+        .should('contain', 'transform: translateX(560px) translateY(229px) translateZ(0px);');
 
       cy.get('.tether-target').click();
 
       // Tether Disabled
       cy.get('.tether-target').should('not.have.class', 'tether-enabled');
       cy.get('.tether-element').should('have.attr', 'style')
-        .should('contain', 'transform: translateX(710px) translateY(229px) translateZ(0px);');
+        .should('contain', 'transform: translateX(560px) translateY(229px) translateZ(0px);');
       cy.get('.container').scrollTo(0, 250);
       cy.get('.tether-element').should('have.attr', 'style')
-        .should('contain', 'transform: translateX(710px) translateY(229px) translateZ(0px);');
+        .should('contain', 'transform: translateX(560px) translateY(229px) translateZ(0px);');
       cy.get('.container').scrollTo(0, 0);
     });
   });
