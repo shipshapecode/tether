49a89dceef8c66c3bce245fd580c17e0f54b2892
Robert Wagner <rwwagner90@gmail.com>
2019-09-19 19:54:12 -0400

    Move deferred utils to their own file

---
+++ test/unit/deferred.spec.js
@@ -0,0 +1,18 @@
+import { defer, flush } from '../../src/js/utils/deferred';
+import { stub } from 'sinon';
+
+describe('deferred', () => {
+  describe('defer/flush', () => {
+    it('calls deferred functions when flush is called', () => {
+      const stub1 = stub();
+      const stub2 = stub();
+      defer(stub1);
+      defer(stub2);
+      expect(stub1.called).toBe(false);
+      expect(stub2.called).toBe(false);
+      flush();
+      expect(stub1.called).toBe(true);
+      expect(stub2.called).toBe(true);
+    });
+  });
+});
