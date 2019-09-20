76f3ccfa0fe73c80f98692d3f335cbabe5b24104
Robert Wagner <rwwagner90@gmail.com>
2019-09-20 09:45:20 -0400

    Fix coverage

--- jest.config.js
+++ jest.config.js
@@ -16,16 +16,13 @@ module.exports = {
   // An array of file extensions your modules use
   moduleFileExtensions: ['js'],
 
-  // The root directory that Jest should scan for tests and modules within
-  rootDir: './',
-
-  // A list of paths to directories that Jest should use to search for files in
-  roots: [
-    '<rootDir>/test/unit'
-  ],
-
   // The path to a module that runs some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: ['<rootDir>/test/unit/setupTests.js'],
+
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/test/cypress/'
+  ],
 
   // A map from regular expressions to paths to transformers
   transform: {
