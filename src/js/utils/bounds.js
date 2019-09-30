1056ab2d4f2191245b736714e27674c9d19645bf
Robert Wagner <rwwagner90@gmail.com>
2019-09-30 08:56:27 -0400

    Move some bounds utils

--- src/js/utils/bounds.js
+++ src/js/utils/bounds.js
@@ -38,6 +38,110 @@ export function getBounds(el) {
   return box;
 }
 
+/**
+ * Gets bounds for when target modifiier is 'scroll-handle'
+ * @param target
+ * @return {{left: number, width: number, height: number}}
+ */
+export function getScrollHandleBounds(target) {
+  let bounds;
+  // We have to do the check for the scrollTop and if target === document.body here and set to variables
+  // because we may reset target below.
+  const targetScrollTop = target.scrollTop;
+  const targetIsBody = target === document.body;
+
+  if (targetIsBody) {
+    target = document.documentElement;
+
+    bounds = {
+      left: pageXOffset,
+      top: pageYOffset,
+      height: innerHeight,
+      width: innerWidth
+    };
+  } else {
+    bounds = getBounds(target);
+  }
+
+  const style = getComputedStyle(target);
+
+  const hasBottomScroll = (
+    target.scrollWidth > target.clientWidth ||
+    [style.overflow, style.overflowX].indexOf('scroll') >= 0 ||
+    !targetIsBody
+  );
+
+  let scrollBottom = 0;
+  if (hasBottomScroll) {
+    scrollBottom = 15;
+  }
+
+  const height = bounds.height - parseFloat(style.borderTopWidth) - parseFloat(style.borderBottomWidth) - scrollBottom;
+
+  const out = {
+    width: 15,
+    height: height * 0.975 * (height / target.scrollHeight),
+    left: bounds.left + bounds.width - parseFloat(style.borderLeftWidth) - 15
+  };
+
+  let fitAdj = 0;
+  if (height < 408 && targetIsBody) {
+    fitAdj = -0.00011 * Math.pow(height, 2) - 0.00727 * height + 22.58;
+  }
+
+  if (!targetIsBody) {
+    out.height = Math.max(out.height, 24);
+  }
+
+  const scrollPercentage = targetScrollTop / (target.scrollHeight - height);
+  out.top = scrollPercentage * (height - out.height - fitAdj) + bounds.top + parseFloat(style.borderTopWidth);
+
+  if (targetIsBody) {
+    out.height = Math.max(out.height, 24);
+  }
+
+  return out;
+}
+
+/**
+ * Gets bounds for when target modifiier is 'visible
+ * @param target
+ * @return {{top: *, left: *, width: *, height: *}}
+ */
+export function getVisibleBounds(target) {
+  if (target === document.body) {
+    return { top: pageYOffset, left: pageXOffset, height: innerHeight, width: innerWidth };
+  } else {
+    const bounds = getBounds(target);
+
+    const out = {
+      height: bounds.height,
+      width: bounds.width,
+      top: bounds.top,
+      left: bounds.left
+    };
+
+    out.height = Math.min(out.height, bounds.height - (pageYOffset - bounds.top));
+    out.height = Math.min(out.height, bounds.height - ((bounds.top + bounds.height) - (pageYOffset + innerHeight)));
+    out.height = Math.min(innerHeight, out.height);
+    out.height -= 2;
+
+    out.width = Math.min(out.width, bounds.width - (pageXOffset - bounds.left));
+    out.width = Math.min(out.width, bounds.width - ((bounds.left + bounds.width) - (pageXOffset + innerWidth)));
+    out.width = Math.min(innerWidth, out.width);
+    out.width -= 2;
+
+    if (out.top < pageYOffset) {
+      out.top = pageYOffset;
+    }
+    if (out.left < pageXOffset) {
+      out.left = pageXOffset;
+    }
+
+    return out;
+  }
+}
+
 export function removeUtilElements() {
   if (zeroElement) {
     document.body.removeChild(zeroElement);
