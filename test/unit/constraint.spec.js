724e4cd86b51d126d1f6ce23021c99d23053ec2b
Robert Wagner <rwwagner90@gmail.com>
2019-10-26 20:30:56 -0400

    Add tests for _calculateOOBAndPinnedTop

--- test/unit/constraint.spec.js
+++ test/unit/constraint.spec.js
@@ -53,7 +53,55 @@ describe('Constraint', () => {
     });
   });
 
-  // describe('_calculateOOBAndPinnedTop', () => {
-  //   const _calculateOOBAndPinnedTop = Constraint.__get__('_calculateOOBAndPinnedTop');
-  // });
+  describe('_calculateOOBAndPinnedTop', () => {
+    const _calculateOOBAndPinnedTop = Constraint.__get__('_calculateOOBAndPinnedTop');
+    const bounds = [10, 10, 20, 20];
+    let oob, pinned, top;
+
+    beforeEach(() => {
+      top = 0;
+      oob = [];
+      pinned = [];
+    });
+
+    it('top < topBound', () => {
+      const pin = [];
+      const height = 10;
+
+      top = _calculateOOBAndPinnedTop(top, bounds, height, pin, pinned, oob);
+      expect(pinned.includes('top'), 'pinned does not include "top"').toBe(false);
+      expect(oob.includes('top'), 'oob includes "top"').toBe(true);
+      expect(top, 'top remains the same').toEqual(0);
+    });
+
+    it('top < topBound: pin top', () => {
+      const pin = ['top'];
+      const height = 10;
+
+      top = _calculateOOBAndPinnedTop(top, bounds, height, pin, pinned, oob);
+      expect(pinned.includes('top'), 'pinned includes "top"').toBe(true);
+      expect(oob.includes('top'), 'oob does not include "top"').toBe(false);
+      expect(top, 'top set to topBound').toEqual(10);
+    });
+
+    it('top + height > bottomBound', () => {
+      const pin = [];
+      const height = 100;
+
+      top = _calculateOOBAndPinnedTop(top, bounds, height, pin, pinned, oob);
+      expect(pinned.includes('bottom'), 'pinned does not include "bottom"').toBe(false);
+      expect(oob.includes('bottom'), 'oob includes "bottom"').toBe(true);
+      expect(top, 'top remains the same').toEqual(0);
+    });
+
+    it('top + height > bottomBound: pin bottom', () => {
+      const pin = ['bottom'];
+      const height = 100;
+
+      top = _calculateOOBAndPinnedTop(top, bounds, height, pin, pinned, oob);
+      expect(pinned.includes('bottom'), 'pinned includes "bottom"').toBe(true);
+      expect(oob.includes('bottom'), 'oob does not "bottom"').toBe(false);
+      expect(top, 'top set to bottomBound - height').toEqual(-80);
+    });
+  });
 });
