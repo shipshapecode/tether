ee5202fab6fec3cdd6a00c2c9c35ad97b8aa2c39
Marika Shanahan <marika@Marikas-MacBook-Pro.local>
2022-06-06 13:43:10 -0700

    named import as index

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'cypress';
+import * as plugins from './test/cypress/plugins';
 
 export default defineConfig({
   fixturesFolder: 'test/cypress/fixtures',
@@ -7,10 +8,10 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./test/cypress/plugins/index.js')(on, config)
+      return plugins(on, config)
     },
     baseUrl: 'http://localhost:9002',
     specPattern: 'test/cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'test/cypress/support/index.js',
-  },
+  }
 })
