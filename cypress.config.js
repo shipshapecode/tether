ae87baf414512e4b63130d1436b7b0b401864305
Marika Shanahan <marika@Marikas-MacBook-Pro.local>
2022-06-06 14:02:04 -0700

    change to cypress ver 10 js config

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,16 @@
-import { defineConfig } from 'cypress';
-import * as plugins from './test/cypress/plugins';
+// import { defineConfig } from 'cypress';
+// import * as plugins from ;
 
-export default defineConfig({
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
   fixturesFolder: 'test/cypress/fixtures',
   video: false,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return plugins(on, config)
+      return require('./test/cypress/plugins')(on, config)
     },
     baseUrl: 'http://localhost:9002',
     specPattern: 'test/cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
